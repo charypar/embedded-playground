e2949eb3424c5e73c373dd6cdce2e4427a175fc4
Viktor Charypar <charypar@gmail.com>
2022-01-23 23:58:41 +0000

    Port to Trinket M0 (ATSAMD21E)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,21 +1,48 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::pac::TC0;
+use bsp::hal;
 use panic_halt as _;
+use trinket_m0 as bsp; // TODO work out how to use the HAL and PAC crates directly
 
-#[arduino_hal::entry]
+use cortex_m_rt::entry;
+use hal::clock::GenericClockController;
+use hal::gpio::v2::{Pins, E};
+use hal::pac::Peripherals;
+use hal::prelude::*;
+use hal::pwm::{Channel, Pwm1};
+
+#[entry]
 fn main() -> ! {
-    let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
+    // Configure peripherals
 
-    let enc_a = pins.d16.into_pull_up_input();
-    let enc_b = pins.d14.into_pull_up_input();
-    let button = pins.d2.into_pull_up_input();
+    let mut peri = Peripherals::take().unwrap();
 
-    let tc0 = dp.TC0;
-    d3_pwm_enable(&tc0, true);
-    pins.d3.into_output();
+    let pins = Pins::new(peri.PORT);
+
+    let enc_a = pins.pa02.into_pull_up_input();
+    let enc_b = pins.pa09.into_pull_up_input();
+    let button = pins.pa07.into_pull_up_input();
+
+    pins.pa06.into_alternate::<E>();
+
+    let mut clocks = GenericClockController::with_internal_32kosc(
+        peri.GCLK,
+        &mut peri.PM,
+        &mut peri.SYSCTRL,
+        &mut peri.NVMCTRL,
+    );
+    let gclk0 = clocks.gclk0();
+
+    let mut pwm1 = Pwm1::new(
+        &clocks.tcc0_tcc1(&gclk0).unwrap(),
+        1.khz(),
+        peri.TCC1,
+        &mut peri.PM,
+    );
+    let max_duty = pwm1.get_max_duty();
+
+    // Program state
 
     let mut led_brightness = 31;
     let mut led_state = true;
@@ -24,15 +51,17 @@ fn main() -> ! {
     let mut enc_state = Enc::NEUTRAL;
 
     loop {
-        let (es, e_out) = read_encoder(enc_a.is_low(), enc_b.is_low(), enc_state);
+        let (es, e_out) = read_encoder(enc_a.is_low().unwrap(), enc_b.is_low().unwrap(), enc_state);
         enc_state = es;
-        let (bs, b_out) = read_button(button.is_high(), button_state);
+        let (bs, b_out) = read_button(button.is_high().unwrap(), button_state);
         button_state = bs;
 
         if b_out {
             led_state = !led_state;
 
-            d3_pwm_enable(&tc0, led_state);
+            if !led_state {
+                pwm1.set_duty(Channel::_0, 0);
+            }
         }
 
         if !led_state {
@@ -44,30 +73,15 @@ fn main() -> ! {
             EncoderOut::CCW if led_brightness > 0 => led_brightness - 1,
             _ => led_brightness,
         };
-        let duty = LED_BRIGHTNESS_MAP[led_brightness];
+        let duty = LED_BRIGHTNESS_MAP[led_brightness] * max_duty / 255;
 
-        d3_pwm_write(&tc0, duty);
+        pwm1.set_duty(Channel::_0, duty);
     }
 }
 
 // Output
 
-fn d3_pwm_enable(tc0: &TC0, enable: bool) {
-    if enable {
-        tc0.tccr0a
-            .write(|w| w.wgm0().pwm_fast().com0b().match_clear());
-        tc0.tccr0b.write(|w| w.cs0().prescale_256());
-    } else {
-        tc0.tccr0a
-            .write(|w| w.wgm0().normal_top().com0b().disconnected());
-    }
-}
-
-fn d3_pwm_write(tc0: &TC0, v: u8) {
-    tc0.ocr0b.write(|w| unsafe { w.bits(v) });
-}
-
-const LED_BRIGHTNESS_MAP: [u8; 64] = [
+const LED_BRIGHTNESS_MAP: [u32; 64] = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 26, 27,
     29, 31, 33, 35, 37, 39, 42, 44, 47, 50, 53, 56, 60, 63, 67, 71, 76, 80, 85, 90, 95, 101, 107,
     114, 120, 128, 135, 143, 152, 161, 170, 180, 191, 203, 215, 227, 241, 255,
