ba1c434b1c9e58916bfb5d546bd210941742d90c
Viktor Charypar <charypar@gmail.com>
2022-10-15 15:05:25 +0100

    Extract usb and bootloader signaling

--- spinny/software/src/main.rs
+++ spinny/software/src/main.rs
@@ -4,7 +4,8 @@
 use panic_halt as _;
 use rtic::app;
 
-mod descriptor;
+mod bootloader;
+mod usb;
 
 #[app(device = stm32f1xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
 mod app {
@@ -16,23 +17,16 @@ mod app {
     use hal::gpio::{Input, Output, PullUp, PushPull};
     use hal::usb::{Peripheral, UsbBus};
 
-    use hal::pac;
-    use pac::{BKP, PWR, RCC, SCB};
-
     use systick_monotonic::{fugit::Duration, Systick};
 
     use usb_device::class_prelude::UsbBusAllocator;
-    use usb_device::device::{UsbDevice, UsbDeviceBuilder, UsbVidPid};
     use usbd_hid::descriptor::SerializedDescriptor;
-    use usbd_hid::hid_class::HIDClass;
-    use usbd_serial::SerialPort;
 
-    use super::descriptor::JoystickReport;
+    use super::bootloader::Bootloader;
+    use super::usb::{Report, Usb};
 
     use cross::debounce::Debounced;
     use cross::rotary::Rotary;
-
-    const RESET_TO_BOOTLOADER: u32 = 0x4F42;
 
     #[monotonic(binds = SysTick, default = true)]
     type RtcMonotonic = Systick<1000>;
@@ -57,19 +51,15 @@ mod app {
 
     #[shared]
     struct Shared {
-        microseconds: u32,
-        usb_device: UsbDevice<'static, UsbBus<Peripheral>>,
-        serial_port: SerialPort<'static, UsbBus<Peripheral>>,
-        hid_device: HIDClass<'static, UsbBus<Peripheral>>,
-        bkp: BKP,
+        usb: Usb<'static, Report>,
+        led: PC13<Output<PushPull>>,
     }
 
     #[local]
     struct Local {
+        bootloader: Bootloader,
         inputs: Inputs,
         inputs_state: InputsState,
-        led: PC13<Output<PushPull>>,
-        last_report: JoystickReport,
     }
 
     #[init(local = [usb_bus: Option<UsbBusAllocator<UsbBus<Peripheral>>> = None])]
@@ -82,7 +72,7 @@ mod app {
         let flash = ctx.device.FLASH;
         let afio = ctx.device.AFIO;
 
-        enable_write_to_bkp(&rcc, &pwr);
+        let bootloader = Bootloader::new(ctx.device.BKP, &rcc, &pwr);
 
         // Configure clocks
         let rcc = rcc.constrain();
@@ -96,34 +86,35 @@ mod app {
         // TODO this can be clocked by a subdivided clock for lower power consumption (probably)
         let rtc = Systick::new(ctx.core.SYST, 36_000_000);
 
-        // Enable USB
-
         let mut gpioa = ctx.device.GPIOA.split();
         let mut gpiob = ctx.device.GPIOB.split();
         let mut gpioc = ctx.device.GPIOC.split();
-
-        let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
-        usb_dp.set_low();
-
-        let usb = Peripheral {
-            usb: ctx.device.USB,
-            pin_dm: gpioa.pa11,
-            pin_dp: usb_dp.into_floating_input(&mut gpioa.crh),
-        };
-
-        let usb_bus = ctx.local.usb_bus.insert(UsbBus::new(usb));
-
-        // End Hardware initialisation
-
-        let (usb_device, serial_port, hid_device) = init_usb(usb_bus);
-
-        // Input
 
         // Disable JTAG to allow access to PB3 and PB4
         let (_, pb3, pb4) = afio
             .constrain()
             .mapr
             .disable_jtag(gpioa.pa15, gpiob.pb3, gpiob.pb4);
+
+        // Enable USB
+
+        // FIXME why is this a thing again?
+        let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+        usb_dp.set_low();
+
+        let usb = UsbBus::new(Peripheral {
+            usb: ctx.device.USB,
+            pin_dm: gpioa.pa11,
+            pin_dp: usb_dp.into_floating_input(&mut gpioa.crh),
+        });
+        let usb_bus = ctx.local.usb_bus.insert(usb);
+
+        let usb = Usb::new(usb_bus, Report::desc());
+
+        // End Hardware initialisation
+
+        // Input
+        // TODO factor this out
 
         let inputs = Inputs {
             buttons: (
@@ -148,38 +139,32 @@ mod app {
             encoder_button: Debounced::new(false, 20),
         };
 
+        let led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
         // Program state
 
-        let shared = Shared {
-            microseconds: 0,
-            usb_device,
-            serial_port,
-            hid_device,
-            bkp: ctx.device.BKP,
-        };
+        let mut shared = Shared { usb, led };
 
-        let mut local = Local {
+        let local = Local {
+            bootloader,
             inputs,
             inputs_state,
-            led: gpioc.pc13.into_push_pull_output(&mut gpioc.crh),
-            last_report: JoystickReport::new(),
         };
 
-        local.led.set_high();
+        shared.led.set_low();
 
         // TODO see if we can do this on a periodic timer instead,
         // so the task execution isn't fallible
-        tick::spawn().unwrap();
+        tick::spawn().ok();
 
         (shared, local, init::Monotonics(rtc))
     }
 
-    #[task(shared = [serial_port, hid_device, microseconds], local = [inputs, inputs_state, led, last_report])]
+    #[task(shared = [usb, led], local = [inputs, inputs_state])]
     fn tick(ctx: tick::Context) {
         let pins = ctx.local.inputs;
         let state = ctx.local.inputs_state;
-        let led = ctx.local.led;
-        let last_report = ctx.local.last_report;
+        let mut led = ctx.shared.led;
 
         // Read inputs and update state
 
@@ -192,7 +177,7 @@ mod app {
         state.buttons[2].update(pins.buttons.2.is_low());
         state.buttons[3].update(pins.buttons.3.is_low());
 
-        led.set_low();
+        led.lock(|led| led.set_low());
 
         // Report state as HID joystick
 
@@ -208,121 +193,39 @@ mod app {
 
         // Debug LEDs
         if buttons.iter().any(|b| *b) {
-            led.set_high()
+            led.lock(|led| led.set_high());
         }
 
-        let mut report = JoystickReport::new();
+        let mut report = Report::new();
 
         for (button, state) in buttons.iter().enumerate() {
             report.set_button(button, *state);
         }
 
-        let mut hid_device = ctx.shared.hid_device;
-
-        if report != *last_report {
-            hid_device.lock(|hid| hid.push_input(&report).ok());
-            *last_report = report;
-        }
+        let mut usb = ctx.shared.usb;
+        usb.lock(|usb| usb.report_input(report)).ok();
 
         // TODO see if we can do this on a periodic timer instead,
         // so the task execution isn't fallible
-        tick::spawn_after(Duration::<u64, 1, 1000>::millis(1)).unwrap();
+        tick::spawn_after(Duration::<u64, 1, 1000>::millis(1)).ok();
     }
 
-    #[task(binds = USB_HP_CAN_TX, priority = 2, shared = [usb_device, serial_port, hid_device, bkp])]
-    fn usb_tx(ctx: usb_tx::Context) {
-        let usb_device = ctx.shared.usb_device;
-        let serial_port = ctx.shared.serial_port;
-        let hid_device = ctx.shared.hid_device;
-        let bkp = ctx.shared.bkp;
-
-        (usb_device, serial_port, hid_device, bkp).lock(|device, serial, hid, bkp| {
-            usb_poll(device, serial, hid, bkp);
-        });
-    }
-
-    #[task(binds = USB_LP_CAN_RX0, priority = 2, shared = [usb_device, serial_port, hid_device, bkp])]
+    #[task(binds = USB_LP_CAN_RX0, priority = 2, shared = [usb])]
     fn usb_rx0(ctx: usb_rx0::Context) {
-        let usb_device = ctx.shared.usb_device;
-        let serial_port = ctx.shared.serial_port;
-        let hid_device = ctx.shared.hid_device;
-        let bkp = ctx.shared.bkp;
+        let mut usb = ctx.shared.usb;
 
-        (usb_device, serial_port, hid_device, bkp).lock(|device, serial, hid, bkp| {
-            usb_poll(device, serial, hid, bkp);
+        usb.lock(|usb| {
+            match usb.poll() {
+                Ok(Some(b'R')) => reset_to_bootloader::spawn().ok(),
+                _ => None,
+            };
         });
     }
 
-    fn usb_poll(
-        device: &mut UsbDevice<UsbBus<Peripheral>>,
-        serial: &mut SerialPort<UsbBus<Peripheral>>,
-        hid: &mut HIDClass<UsbBus<Peripheral>>,
-        bkp: &mut BKP,
-    ) {
-        device.poll(&mut [serial, hid]);
+    #[task(local = [bootloader])]
+    fn reset_to_bootloader(ctx: reset_to_bootloader::Context) {
+        let bootloader = ctx.local.bootloader;
 
-        // Prevent writes into the serial port locking everthing up
-        // I am honestly not sure why this happens.
-        let mut buf = [0u8; 1];
-        match serial.read(&mut buf) {
-            Ok(0) => (),
-            Ok(count) => {
-                // TODO better reset command
-                if buf[0] == b'R' {
-                    if signal_bootloader(bkp) {
-                        serial.write(b"resetting to bootloader!").ok();
-
-                        SCB::sys_reset();
-                    } else {
-                        serial.write(b"reset failed").ok();
-                    }
-                } else {
-                    serial.write(&buf[0..count]).ok();
-                }
-            }
-            Err(_) => (),
-        }
-    }
-
-    // Enable writing into the BKP registers for software reset to bootloader
-    fn enable_write_to_bkp(rcc: &RCC, pwr: &PWR) {
-        // From stm32 reference manual, pg 81:
-        // 1. enable the power and backup interface clocks
-        // 2. set the DBP bit in the Power control register
-
-        rcc.apb1enr.write(|w| w.pwren().set_bit().bkpen().set_bit());
-        pwr.cr.write(|w| w.dbp().set_bit());
-    }
-
-    fn signal_bootloader(bkp: &mut BKP) -> bool {
-        // from dapboot documentation/source code
-        // https://github.com/devanlai/dapboot#switching-to-the-bootloader
-        // https://github.com/devanlai/dapboot/blob/master/src/stm32f103/backup.c#L25
-        //
-        // 3. write the bootloader signal
-        bkp.dr[0].write(|w| unsafe { w.bits(RESET_TO_BOOTLOADER) });
-
-        // check the write succeeded
-        return bkp.dr[0].read().bits() == RESET_TO_BOOTLOADER;
-    }
-
-    fn init_usb<'a>(
-        allocator: &'a UsbBusAllocator<UsbBus<Peripheral>>,
-    ) -> (
-        UsbDevice<'a, UsbBus<Peripheral>>,
-        SerialPort<'a, UsbBus<Peripheral>>,
-        HIDClass<'a, UsbBus<Peripheral>>,
-    ) {
-        let serial = SerialPort::new(allocator);
-        let hid_device = HIDClass::new_ep_in(allocator, JoystickReport::desc(), 4);
-
-        // https://github.com/obdev/v-usb/blob/master/usbdrv/USB-IDs-for-free.txt
-        let usb_device = UsbDeviceBuilder::new(allocator, UsbVidPid(0x16c0, 0x27dc))
-            .manufacturer("Niche http://niche.london/")
-            .product("Spinny development board")
-            .serial_number("niche.london:Spinny-v0.1")
-            .build();
-
-        (usb_device, serial, hid_device)
+        bootloader.reset();
     }
 }
---
+++ spinny/software/src/usb.rs
@@ -0,0 +1,116 @@
+use hal::usb::{Peripheral, UsbBus};
+use stm32f1xx_hal as hal;
+
+use usb_device::class_prelude::UsbBusAllocator;
+use usb_device::device::{UsbDevice, UsbDeviceBuilder, UsbVidPid};
+use usb_device::UsbError;
+use usbd_hid::descriptor::AsInputReport;
+use usbd_hid::hid_class::HIDClass;
+use usbd_serial::SerialPort;
+
+use usbd_hid::descriptor::gen_hid_descriptor;
+use usbd_hid::descriptor::generator_prelude::*;
+
+#[gen_hid_descriptor(
+    (collection = APPLICATION, usage_page = GENERIC_DESKTOP, usage = JOYSTICK) = {
+        (collection = PHYSICAL, usage = JOYSTICK) = {
+            (usage_page = BUTTON, usage_min = BUTTON_1, usage_max = 0x07) = {
+                #[packed_bits 7] #[item_settings data,variable,absolute] buttons=input;
+            };
+        }
+    }
+)]
+#[derive(PartialEq)]
+pub struct Report {
+    pub buttons: u8,
+}
+
+impl Report {
+    pub fn new() -> Self {
+        Self { buttons: 0 }
+    }
+
+    pub fn set_button(&mut self, n: usize, value: bool) {
+        if n > 6 {
+            return;
+        }
+
+        if value {
+            self.buttons |= 1 << n
+        } else {
+            self.buttons &= !(1 << n)
+        }
+    }
+}
+
+// USB Interface
+pub struct Usb<'a, IR>
+where
+    IR: AsInputReport + PartialEq<IR>,
+{
+    device: UsbDevice<'a, UsbBus<Peripheral>>,
+    serial: SerialPort<'a, UsbBus<Peripheral>>,
+    hid: HIDClass<'a, UsbBus<Peripheral>>,
+    last_report: Option<IR>, // Last input report sent
+}
+
+impl<'a, IR> Usb<'a, IR>
+where
+    IR: AsInputReport + PartialEq<IR>,
+{
+    pub fn new(
+        allocator: &'a UsbBusAllocator<UsbBus<Peripheral>>,
+        report_descriptor: &'static [u8],
+    ) -> Usb<'a, IR> {
+        let serial_port = SerialPort::new(allocator);
+        let hid_device = HIDClass::new_ep_in(allocator, report_descriptor, 4);
+
+        // https://github.com/obdev/v-usb/blob/master/usbdrv/USB-IDs-for-free.txt
+        // TODO expose the string settings
+        let usb_device = UsbDeviceBuilder::new(allocator, UsbVidPid(0x16c0, 0x27dc))
+            .manufacturer("Niche http://niche.london/")
+            .product("Spinny development board")
+            .serial_number("niche.london:Spinny-v0.1")
+            .build();
+
+        Usb {
+            device: usb_device,
+            serial: serial_port,
+            hid: hid_device,
+            last_report: None,
+        }
+    }
+
+    pub fn report_input(&mut self, report: IR) -> Result<usize, UsbError> {
+        // Skip the report if it's the same as the last one
+        if let Some(last_report) = &self.last_report {
+            if *last_report == report {
+                return Ok(0);
+            }
+        }
+
+        let bytes = self.hid.push_input(&report)?;
+        self.last_report = Some(report);
+
+        Ok(bytes)
+    }
+
+    // FIXME think this interface through properly...
+    pub fn poll(&mut self) -> Result<Option<u8>, UsbError> {
+        self.device.poll(&mut [&mut self.serial, &mut self.hid]);
+
+        // Prevent writes into the serial port locking everthing up
+        // I am honestly not sure why this happens.
+        let mut buf = [0u8; 1];
+
+        match self.serial.read(&mut buf) {
+            Ok(0) => Ok(None),
+            Ok(count) => {
+                self.serial.write(&buf[0..count])?;
+
+                return Ok(Some(buf[0]));
+            }
+            Err(_) => Ok(None),
+        }
+    }
+}
