9014750dd0c4ea99898bce6c934baa5fa46dd96c
Viktor Charypar <charypar@gmail.com>
2022-10-15 16:32:51 +0100

    Factor out stm32 specific peripheral setup

---
+++ spinny/software/src/device.rs
@@ -0,0 +1,142 @@
+use hal::prelude::*;
+use stm32f1xx_hal as hal;
+
+use hal::pac;
+use pac::{BKP, PWR, RCC, SCB};
+
+use hal::gpio::gpiob::{PB3, PB4, PB5, PB6, PB7, PB8, PB9};
+use hal::gpio::gpioc::PC13;
+use hal::gpio::{GpioExt, Input, Output, PullUp, PushPull};
+use hal::pac::Peripherals;
+use hal::usb::{Peripheral, UsbBus};
+
+use usb_device::class_prelude::UsbBusAllocator;
+
+pub struct InputPins {
+    pins: (
+        PB3<Input<PullUp>>,
+        PB4<Input<PullUp>>,
+        PB5<Input<PullUp>>,
+        PB6<Input<PullUp>>,
+        PB7<Input<PullUp>>,
+        PB8<Input<PullUp>>,
+        PB9<Input<PullUp>>,
+    ),
+}
+
+impl InputPins {
+    pub fn scan(&self) -> [bool; 7] {
+        [
+            self.pins.0.is_low(),
+            self.pins.1.is_low(),
+            self.pins.2.is_low(),
+            self.pins.3.is_low(),
+            self.pins.4.is_low(),
+            self.pins.5.is_low(),
+            self.pins.6.is_low(),
+        ]
+    }
+}
+
+pub struct Device {
+    pub bootloader: Bootloader,
+    pub pins: InputPins,
+    pub led: PC13<Output<PushPull>>,
+    pub usb: UsbBusAllocator<UsbBus<Peripheral>>,
+}
+
+impl Device {
+    pub fn new(device: Peripherals) -> Self {
+        let rcc = device.RCC;
+        let pwr = device.PWR;
+        let flash = device.FLASH;
+        let afio = device.AFIO;
+
+        let bootloader = Bootloader::new(device.BKP, &rcc, &pwr);
+
+        // Configure clocks
+        let rcc = rcc.constrain();
+        let mut acr = flash.constrain().acr;
+        rcc.cfgr
+            .use_hse(8.MHz()) // External crystal is on 8 Mhz
+            .sysclk(72.MHz()) // Clock system on 72 Mhz, used also by USB, divided by 1.5 to 48 MHz
+            .freeze(&mut acr);
+
+        let mut gpioa = device.GPIOA.split();
+        let mut gpiob = device.GPIOB.split();
+        let mut gpioc = device.GPIOC.split();
+
+        // Disable JTAG to allow access to PB3 and PB4
+        let (_, pb3, pb4) = afio
+            .constrain()
+            .mapr
+            .disable_jtag(gpioa.pa15, gpiob.pb3, gpiob.pb4);
+
+        // Enable USB
+
+        // FIXME why is this a thing again?
+        let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+        usb_dp.set_low();
+
+        let usb = UsbBus::new(Peripheral {
+            usb: device.USB,
+            pin_dm: gpioa.pa11,
+            pin_dp: usb_dp.into_floating_input(&mut gpioa.crh),
+        });
+
+        let pb3 = pb3.into_pull_up_input(&mut gpiob.crl);
+        let pb4 = pb4.into_pull_up_input(&mut gpiob.crl);
+        let pb5 = gpiob.pb5.into_pull_up_input(&mut gpiob.crl);
+        let pb6 = gpiob.pb6.into_pull_up_input(&mut gpiob.crl);
+        let pb7 = gpiob.pb7.into_pull_up_input(&mut gpiob.crl);
+        let pb8 = gpiob.pb8.into_pull_up_input(&mut gpiob.crh);
+        let pb9 = gpiob.pb9.into_pull_up_input(&mut gpiob.crh);
+
+        let led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+        Device {
+            pins: InputPins {
+                pins: (pb3, pb4, pb5, pb6, pb7, pb8, pb9),
+            },
+            bootloader,
+            led,
+            usb,
+        }
+    }
+}
+
+// A convenient dapboot bootloader interface
+pub struct Bootloader {
+    bkp: BKP,
+}
+
+impl Bootloader {
+    const RESET_TO_BOOTLOADER: u32 = 0x4F42;
+
+    // Create a bootloader inteface and enable signaling
+    pub fn new(bkp: BKP, rcc: &RCC, pwr: &PWR) -> Self {
+        // From stm32 reference manual, pg 81:
+        // 1. enable the power and backup interface clocks
+        // 2. set the DBP bit in the Power control register
+
+        rcc.apb1enr.write(|w| w.pwren().set_bit().bkpen().set_bit());
+        pwr.cr.write(|w| w.dbp().set_bit());
+
+        Self { bkp }
+    }
+
+    // Restart the device into bootloader
+    pub fn reset(&self) {
+        // from dapboot documentation/source code
+        // https://github.com/devanlai/dapboot#switching-to-the-bootloader
+        // https://github.com/devanlai/dapboot/blob/master/src/stm32f103/backup.c#L25
+        //
+        // 3. write the bootloader signal
+        self.bkp.dr[0].write(|w| unsafe { w.bits(Self::RESET_TO_BOOTLOADER) });
+
+        // check the write succeeded
+        if self.bkp.dr[0].read().bits() == Self::RESET_TO_BOOTLOADER {
+            SCB::sys_reset();
+        }
+    }
+}
--- spinny/software/src/main.rs
+++ spinny/software/src/main.rs
@@ -4,17 +4,15 @@
 use panic_halt as _;
 use rtic::app;
 
-mod bootloader;
+mod device;
 mod usb;
 
 #[app(device = stm32f1xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
 mod app {
-    use hal::prelude::*;
     use stm32f1xx_hal as hal;
 
-    use hal::gpio::gpiob::{PB3, PB4, PB5, PB6, PB7, PB8, PB9};
     use hal::gpio::gpioc::PC13;
-    use hal::gpio::{Input, Output, PullUp, PushPull};
+    use hal::gpio::{Output, PushPull};
     use hal::usb::{Peripheral, UsbBus};
 
     use systick_monotonic::{fugit::Duration, Systick};
@@ -22,8 +20,8 @@ mod app {
     use usb_device::class_prelude::UsbBusAllocator;
     use usbd_hid::descriptor::SerializedDescriptor;
 
-    use super::bootloader::Bootloader;
-    use super::usb::{Report, Usb};
+    use crate::device::{Bootloader, Device, InputPins};
+    use crate::usb::{Report, Usb};
 
     use cross::debounce::Debounced;
     use cross::rotary::Rotary;
@@ -37,18 +35,6 @@ mod app {
         encoder_button: Debounced<bool>,
     }
 
-    pub struct Inputs {
-        buttons: (
-            PB3<Input<PullUp>>,
-            PB4<Input<PullUp>>,
-            PB6<Input<PullUp>>,
-            PB7<Input<PullUp>>,
-        ),
-        encoder_button: PB5<Input<PullUp>>,
-        encoder_a: PB9<Input<PullUp>>,
-        encoder_b: PB8<Input<PullUp>>,
-    }
-
     #[shared]
     struct Shared {
         usb: Usb<'static, UsbBus<Peripheral>, Report>,
@@ -58,7 +44,7 @@ mod app {
     #[local]
     struct Local {
         bootloader: Bootloader,
-        inputs: Inputs,
+        pins: InputPins,
         inputs_state: InputsState,
     }
 
@@ -67,66 +53,19 @@ mod app {
         // Begin hardware initialisation
         // FIXME pull all of this out
 
-        let rcc = ctx.device.RCC;
-        let pwr = ctx.device.PWR;
-        let flash = ctx.device.FLASH;
-        let afio = ctx.device.AFIO;
+        let device = Device::new(ctx.device);
 
-        let bootloader = Bootloader::new(ctx.device.BKP, &rcc, &pwr);
-
-        // Configure clocks
-        let rcc = rcc.constrain();
-        let mut acr = flash.constrain().acr;
-        rcc.cfgr
-            .use_hse(8.MHz()) // External crystal is on 8 Mhz
-            .sysclk(72.MHz()) // Clock system on 72 Mhz, used also by USB, divided by 1.5 to 48 MHz
-            .freeze(&mut acr);
+        let usb_bus = ctx.local.usb_bus.insert(device.usb);
+        let usb = Usb::new(usb_bus, Report::desc());
 
         // Set up the real-time clock, using the 32K oscillator clock
         // TODO this can be clocked by a subdivided clock for lower power consumption (probably)
         let rtc = Systick::new(ctx.core.SYST, 36_000_000);
 
-        let mut gpioa = ctx.device.GPIOA.split();
-        let mut gpiob = ctx.device.GPIOB.split();
-        let mut gpioc = ctx.device.GPIOC.split();
-
-        // Disable JTAG to allow access to PB3 and PB4
-        let (_, pb3, pb4) = afio
-            .constrain()
-            .mapr
-            .disable_jtag(gpioa.pa15, gpiob.pb3, gpiob.pb4);
-
-        // Enable USB
-
-        // FIXME why is this a thing again?
-        let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
-        usb_dp.set_low();
-
-        let usb = UsbBus::new(Peripheral {
-            usb: ctx.device.USB,
-            pin_dm: gpioa.pa11,
-            pin_dp: usb_dp.into_floating_input(&mut gpioa.crh),
-        });
-        let usb_bus = ctx.local.usb_bus.insert(usb);
-
-        let usb = Usb::new(usb_bus, Report::desc());
-
         // End Hardware initialisation
 
         // Input
         // TODO factor this out
-
-        let inputs = Inputs {
-            buttons: (
-                pb3.into_pull_up_input(&mut gpiob.crl),
-                pb4.into_pull_up_input(&mut gpiob.crl),
-                gpiob.pb6.into_pull_up_input(&mut gpiob.crl),
-                gpiob.pb7.into_pull_up_input(&mut gpiob.crl),
-            ),
-            encoder_button: gpiob.pb5.into_pull_up_input(&mut gpiob.crl),
-            encoder_a: gpiob.pb9.into_pull_up_input(&mut gpiob.crh),
-            encoder_b: gpiob.pb8.into_pull_up_input(&mut gpiob.crh),
-        };
 
         let inputs_state = InputsState {
             buttons: [
@@ -139,16 +78,17 @@ mod app {
             encoder_button: Debounced::new(false, 20),
         };
 
-        let led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-
         // Program state
 
-        let mut shared = Shared { usb, led };
+        let mut shared = Shared {
+            led: device.led,
+            usb,
+        };
 
         let local = Local {
-            bootloader,
-            inputs,
+            bootloader: device.bootloader,
             inputs_state,
+            pins: device.pins,
         };
 
         shared.led.set_low();
@@ -160,22 +100,25 @@ mod app {
         (shared, local, init::Monotonics(rtc))
     }
 
-    #[task(shared = [usb, led], local = [inputs, inputs_state])]
+    #[task(shared = [usb, led], local = [pins, inputs_state])]
     fn tick(ctx: tick::Context) {
-        let pins = ctx.local.inputs;
+        let pins = ctx.local.pins;
         let state = ctx.local.inputs_state;
         let mut led = ctx.shared.led;
 
         // Read inputs and update state
 
-        state.encoder_button.update(pins.encoder_button.is_low());
-        let (a, b) = (pins.encoder_a.is_low(), pins.encoder_b.is_low());
-        state.encoder.update(a, b);
+        let pin_state = pins.scan();
 
-        state.buttons[0].update(pins.buttons.0.is_low());
-        state.buttons[1].update(pins.buttons.1.is_low());
-        state.buttons[2].update(pins.buttons.2.is_low());
-        state.buttons[3].update(pins.buttons.3.is_low());
+        // FIXME centralise the pin mapping and make it less error prone
+        // Ideally map pin type straight to an Encoder, Button or Switch
+        state.encoder_button.update(pin_state[2]);
+        state.encoder.update(pin_state[6], pin_state[5]);
+
+        state.buttons[0].update(pin_state[0]);
+        state.buttons[1].update(pin_state[1]);
+        state.buttons[2].update(pin_state[3]);
+        state.buttons[3].update(pin_state[4]);
 
         led.lock(|led| led.set_low());
 
@@ -191,7 +134,7 @@ mod app {
             state.encoder_button.get(),
         ];
 
-        // Debug LEDs
+        // Debug LED
         if buttons.iter().any(|b| *b) {
             led.lock(|led| led.set_high());
         }
